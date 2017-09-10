996a639afe1ead52730ef275823365da98cbd8f1
faisolp <faisolp@gmail.com>
2017-09-10 13:46:13 +0700

    UI OK

---
+++ IOSToolsBar/TimerViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TimerViewController.swift
+//  IOSToolsBar
+//
+//  Created by Faisol Phalawon on 9/10/2560 BE.
+//  Copyright © 2560 FaisolPhalawon. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    //---------------------------------
+    
+    @IBOutlet weak var labCount: UILabel!
+    
+    
+    //--------------------------------
+    
+    
+    @IBAction func btnPlay(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnStop(_ sender: Any) {
+    }
+    
+    //---------------------------------
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
